f3af69ac6fbd6ee405c4bc9111467237a6fb9f95
sherodesigns5 <yun@sherodesigns.com>
2016-01-05 12:17:33 -0500

    Initial commit

---
+++ dev.trackingdogsupply.com/html/app/etc/env.php
@@ -0,0 +1,61 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin_1tyyln',
+  ),
+  'install' => 
+  array (
+    'date' => 'Thu, 17 Dec 2015 22:46:21 +0000',
+  ),
+  'crypt' => 
+  array (
+    'key' => '1435d0a24e77cd8a9a002c6ab5548983',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'tracking_dev',
+        'username' => 'tracking_dev',
+        'password' => 'KiltCuriesGirdleTalon60',
+        'model' => 'mysql4',
+        'engine' => 'innodb',
+        'initStatements' => 'SET NAMES utf8;',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 0,
+    'layout' => 0,
+    'block_html' => 0,
+    'collections' => 0,
+    'reflection' => 0,
+    'db_ddl' => 0,
+    'eav' => 0,
+    'config_integration' => 0,
+    'config_integration_api' => 0,
+    'full_page' => 0,
+    'translate' => 0,
+    'config_webservice' => 0,
+  ),
+);
